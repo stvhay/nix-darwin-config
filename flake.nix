071c26f8aef4b2810205b97eb411c019d7f5b54d
Steve Hay <github@stevenhay.com>
2025-09-29 21:03:14 +0200

    Update on Mon Sep 29 21:03:14 CEST 2025.

--- flake.nix
+++ flake.nix
@@ -88,6 +88,7 @@
       fonts.packages = with pkgs; [
         fira-code
         fira-code-symbols
+        nerd-fonts.jetbrains-mono
         mplus-outline-fonts.githubRelease
         noto-fonts
         noto-fonts-emoji
