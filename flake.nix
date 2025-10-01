6ae186a2e3adea01ab2b87c5c2571934762dce49
Steve Hay <github@stevenhay.com>
2025-10-01 19:55:13 -0400

    Update on Wed Oct  1 19:55:13 EDT 2025.

--- flake.nix
+++ flake.nix
@@ -131,6 +131,11 @@
         ];
 
         masApps = {
+          "Logic Pro" = 634148309;
+          "Microsoft Remote Desktop" = 1295203466;
+          MindNode = 1289197285;
+          TestFlight = 899247664;
+          "The Unarchiver" = 425424353;
           WireGuard = 1451685025;
         };
       };
