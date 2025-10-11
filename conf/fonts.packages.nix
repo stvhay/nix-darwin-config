d27f7302f21e74b8fb52e7716d2f6a26747a0687
Steve Hay <github@stevenhay.com>
2025-10-10 22:39:34 -0400

    add some fonts.

--- conf/fonts.packages.nix
+++ conf/fonts.packages.nix
@@ -1,9 +1,13 @@
 pkgs: with pkgs;
 [
+  cantarell-fonts
   fira-code
   fira-code-symbols
-  nerd-fonts.jetbrains-mono
+  hack-font
+  monaspace
   mplus-outline-fonts.githubRelease
+  nerd-fonts.jetbrains-mono
+  nerd-fonts.droid-sans-mono
   noto-fonts
   noto-fonts-emoji
 ]
