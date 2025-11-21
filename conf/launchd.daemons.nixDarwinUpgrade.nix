92f3fa0f4407975336d1672245dbebdf0d65edf2
Steve Hay <github@stevenhay.com>
2025-11-21 00:52:50 -0500

    added homebrew cask update

--- conf/launchd.daemons.nixDarwinUpgrade.nix
+++ conf/launchd.daemons.nixDarwinUpgrade.nix
@@ -3,13 +3,11 @@ pkgs: with pkgs;
   script = ''
     printf --  "----------\n%s\n----------\n\n" "$(date)"
     chown -R root:wheel /etc/nix-darwin
-    printf "NIX-CHANNEL\n" >&2
-    /usr/bin/sudo -u hays /run/current-system/sw/bin/nix-channel --update
-    printf "NIX FLAKE UPDATE\n" >&2
-    /usr/bin/sudo -u hays /run/current-system/sw/bin/nix flake update --commit-lock-file 
-    printf "DARWIN-REBUILD\n" >&2
-    /run/current-system/sw/bin/darwin-rebuild switch --flake .
+    /usr/bin/sudo -u hays /run/current-system/sw/bin/nix-channel --update 2>&1
+    /usr/bin/sudo -u hays /run/current-system/sw/bin/nix flake update --commit-lock-file 2>&1
+    /run/current-system/sw/bin/darwin-rebuild switch --flake . 2>&1
     chown -R hays:admin /etc/nix-darwin
+    /usr/bin/sudo -i -u hays bash --login -c "brew upgrade" 2>&1
   '';
   serviceConfig = {
     StartInterval = 86400; # every 24 hours
