08fdc42cde1893f21a2c1a59df4a5544b5b4d6ed
Steve Hay <github@stevenhay.com>
2026-01-21 21:20:46 -0500

    back to 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
